0fe1ca8b27ba5daa7afee527b25ff921e7bb8ddf
unknown <helf4ch@yandex.ru>
2025-03-05 20:41:55 +1000

    init commit

---
+++ src/test/java/ru/helf4ch/textstat/TextstatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.helf4ch.textstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
