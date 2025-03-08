b768cce315e68f6d99937b1bbd36c4bde2a39be8
helf4ch <helf4ch@yandex.ru>
2025-03-08 17:35:44 +1000

    feat: changed file structure

--- src/main/java/com/github/helf4ch/textstat/TextstatApplication.java
+++ src/main/java/com/github/helf4ch/textstat/TextstatApplication.java
@@ -1,4 +1,4 @@
-package ru.helf4ch.textstat;
+package com.github.helf4ch.textstat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TextstatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TextstatApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TextstatApplication.class, args);
+  }
 }
--- src/test/java/com/github/helf4ch/textstat/TextstatApplicationTests.java
+++ src/test/java/com/github/helf4ch/textstat/TextstatApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.helf4ch.textstat;
+package com.github.helf4ch.textstat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
